e47113fe8fa0b2f4da5a9b75eb82f05ce21ba477
DSQTeam <124442520+DSQTeam@users.noreply.github.com>
2024-03-20 19:59:00 +0300

    Add canvas for main menu
    Select_Level button works,
    need UI art

---
+++ Butterfly/Assets/Select_Level.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Select_Level : MonoBehaviour
+{
+    public void Choose_Level()
+    {
+        SceneManager.LoadSceneAsync(2);
+
+    }
+    
+    
+}
