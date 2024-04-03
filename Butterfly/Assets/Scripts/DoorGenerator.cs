58f796eace877debb7aa0764492c6dafd7e4890d
DSQTeam <124442520+DSQTeam@users.noreply.github.com>
2024-04-04 02:41:38 +0300

    Add door generator(needs ungrades)
    I hate lists

---
+++ Butterfly/Assets/Scripts/DoorGenerator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objectsToSpawn = new List<GameObject>(); // Лист объектов, которые вы хотите спавнить 
+    public List<Transform> spawnPoints = new List<Transform>();     // Лист точек спавна 
+    private void Start()
+    {
+        SpawnRandomObject();
+    }
+
+    void SpawnRandomObject()
+    {
+        
+        foreach (GameObject obj in objectsToSpawn )
+        {
+            // Выбираем случайную точку спавна 
+             int randomSpawnPointIndex = Random.Range(0, spawnPoints.Count);
+            Transform spawnPoint = spawnPoints[randomSpawnPointIndex];
+
+            // Выбираем случайный объект для спавна 
+             int randomObjectIndex = Random.Range(0, objectsToSpawn.Count);
+            GameObject objectToSpawn = objectsToSpawn[randomObjectIndex];
+
+            // Создаем объект на выбранной точке спавна 
+            Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+
+            
+        }
+
+    }
+
+}
--- Butterfly/Assets/Scripts/Select_Level.cs
+++ Butterfly/Assets/Scripts/Select_Level.cs
@@ -6,7 +6,7 @@ public class Select_Level : MonoBehaviour
 {
     public void Choose_Level()
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(1);
 
     }
     
