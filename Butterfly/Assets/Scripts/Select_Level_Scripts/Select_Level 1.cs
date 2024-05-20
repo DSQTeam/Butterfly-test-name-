5f0381de46d0d028156157ccee18c4660416ba81
Alkatrasik <124365261+Alkatrasik@users.noreply.github.com>
2024-05-20 19:57:34 +0400

    Beta-1

---
+++ Butterfly/Assets/Scripts/Select_Level_Scripts/Select_Level 1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Select_Level1 : MonoBehaviour
+{
+    public void Level_selection1()
+    {
+        SceneManager.LoadSceneAsync(3);
+
+    }
+
+}
---
+++ Butterfly/Assets/Scripts/Select_Level_Scripts/Select_Level_back 4.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Select_Level_back4 : MonoBehaviour
+{
+    public void Level_selection_back4()
+    {
+        SceneManager.LoadSceneAsync(5);
+
+    }
+
+}
